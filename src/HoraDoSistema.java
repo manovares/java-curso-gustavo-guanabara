e662ece02d03e0ceb8db4f5350344eff312cafc7
manovares <rm566844@fiap.com.br>
2026-04-05 17:05:19 -0300

    usando date java

---
+++ src/HoraDoSistema.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+
+public class HoraDoSistema {
+    static void main() {
+        Date relogio = new Date();
+        System.out.println("A hora do sistame é");
+        System.out.println(relogio.toString());
+    }
+}
