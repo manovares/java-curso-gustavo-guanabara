e41f2e446437585924102c645a49f0c74f509b96
manovares <rm566844@fiap.com.br>
2026-04-08 13:24:31 -0300

    aprendendo sobre operadores java

---
+++ src/OperadoresAritmetricos/Operadores.java
@@ -0,0 +1,14 @@
+package OperadoresAritmetricos;
+
+public class Operadores {
+    public static void main(String[] args) {
+        int n1 = 3;
+        int n2 = 5;
+        float media = (n1 + n2)/2;
+        System.out.println("media = " + media);
+
+        int numero = 5;
+        numero++;
+        System.out.println("numero = " + numero);
+    }
+}
